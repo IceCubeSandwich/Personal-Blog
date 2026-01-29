8b4efbd523d2f21331ae833f0e6539cfc0b5de3c
Tyrrell | 0xtb <50052035+IceCubeSandwich@users.noreply.github.com>
2026-01-29 18:59:44 -0500

    Update social media links in site configuration

--- site.config.ts
+++ site.config.ts
@@ -17,12 +17,12 @@ export default siteConfig({
   description: 'Silent channels, loud impact.',
   
   // social usernames (optional)
-  twitter: '0xtb_',
+  // twitter: '0xtb_',
   github: 'IceCubeSandwich',
   linkedin: 'tyrrell-brewster',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'channel/UCJnMJ83rloUPb2O1u8MAASw?sub_confirmation=1', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
   
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
